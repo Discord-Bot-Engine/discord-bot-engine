88553a2af6cdec7ce8fb3a795a030f8310b28b22
Caldes Mihai <mihaicaldes91@gmail.com>
2025-09-28 14:42:54 +0300

    bug fix

--- src/routes/marketplace/+page.svelte
+++ src/routes/marketplace/+page.svelte
@@ -4,13 +4,14 @@
 	import Plugin from "$lib/components/Plugin.svelte";
 	import {Input} from "$lib/components/ui/input/index.js";
 	let name = $state("")
+	let plugins = $derived(PluginManager.plugins.filter(p => p.name.toLowerCase().includes(name.toLowerCase())).sort((a, b) => a.name.localeCompare(b.name)))
 </script>
 <div class="w-full p-3 pb-0">
-	<Input value={name} placeholder="Search plugin..."/>
+	<Input bind:value={name} placeholder="Search plugin..."/>
 </div>
 <ScrollArea>
 	<div class="w-full h-full grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-3 gap-3">
-		{#each PluginManager.plugins.filter(p => p.name.toLowerCase().includes(name.toLowerCase())).sort((a, b) => a.name.localeCompare(b.name)) as plugin}
+		{#each plugins as plugin}
 			<Plugin {plugin}/>
 		{/each}
 	</div>
