46c5f29bd7bfb9febeca1bc2a287830f8ee7f57f
Caldes Mihai <mihaicaldes91@gmail.com>
2025-10-25 11:47:38 +0300

    bug fix

--- src/lib/components/Run.svelte
+++ src/lib/components/Run.svelte
@@ -10,8 +10,8 @@
     <Card.Content class="p-1 overflow-hidden h-full">
         <div class="flex justify-end h-fit">
             <label class="mr-auto opacity-50 text-md">Console</label>
-            <Button variant={BotManager.selectedBot?.isRunning ? undefined : 'outline'} size="icon" class="size-8 rounded-lg rounded-r-none" onclick={() => BotManager.runBot()}><PlayIcon size={16} /></Button>
-            <Button variant={BotManager.selectedBot?.isRunning ? 'outline' : undefined} size="icon" class="size-8 rounded-lg rounded-l-none" onclick={() => BotManager.stopBot()}><SquareIcon size={16} /></Button>
+            <Button variant={BotManager.selectedBot?.isRunning ? undefined : 'secondary'} size="icon" class="size-8 rounded-lg rounded-r-none" onclick={() => BotManager.runBot()}><PlayIcon size={16} /></Button>
+            <Button variant={BotManager.selectedBot?.isRunning ? 'secondary' : undefined} size="icon" class="size-8 rounded-lg rounded-l-none" onclick={() => BotManager.stopBot()}><SquareIcon size={16} /></Button>
         </div>
         <ScrollArea class="font-[JetBrains_Mono_Variable] text-sm h-[calc(100%-1.8rem)]">
             <pre class="whitespace-pre-wrap">{BotManager.selectedBot?.stdout}</pre>
