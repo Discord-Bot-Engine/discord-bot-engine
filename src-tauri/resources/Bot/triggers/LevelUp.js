b66a43d9bc49501d48d018f848e2d9e5e8bbd535
Caldes Mihai <mihaicaldes91@gmail.com>
2025-10-25 11:36:53 +0300

    bug fix

---
+++ src-tauri/resources/Bot/triggers/LevelUp.js
@@ -0,0 +1,35 @@
+import {Events, REST, Routes, SlashCommandBuilder} from "discord.js";
+import {Bot} from "../classes/Bot.js"
+
+export default class LevelUp {
+    static type = "Level Up"
+    static variableTypes = ["Message", "Member", "Channel", "Server"]
+    static event = "levelUp"
+    static runIf = () => true
+    static html = `
+        <div class="grid grid-cols-4 items-center gap-4">
+            <dbe-label name="Store message in variable"></dbe-label>
+            <dbe-variable-list name="message" class="col-span-3" variableType="Message"></dbe-variable-list>
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+            <dbe-label name="Store member in variable"></dbe-label>
+            <dbe-variable-list name="member" class="col-span-3" variableType="Member"></dbe-variable-list>
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+            <dbe-label name="Store channel in variable"></dbe-label>
+            <dbe-variable-list name="channel" class="col-span-3" variableType="Channel"></dbe-variable-list>
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+            <dbe-label name="Store server in variable"></dbe-label>
+            <dbe-variable-list name="server" class="col-span-3" variableType="Server"></dbe-variable-list>
+        </div>
+    `
+    static load({data, actionManager, setVariable}) {}
+    static run({data, actionManager, setVariable}, message) {
+        setVariable(data.get("message"), message);
+        setVariable(data.get("member"), message.member);
+        setVariable(data.get("channel"), message.channel);
+        setVariable(data.get("server"), message.guild);
+        actionManager.runNext()
+    }
+}
