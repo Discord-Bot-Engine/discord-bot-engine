591571fdf1ba142dc63db0977199eefef7cf95f0
Caldes Mihai <mihaicaldes91@gmail.com>
2025-11-18 16:34:16 +0200

    translate

--- src-tauri/resources/Bot/actions/StoreList.js
+++ src-tauri/resources/Bot/actions/StoreList.js
@@ -6,11 +6,18 @@ export default class StoreList {
             <dbe-label name="Store list in variable"></dbe-label>
             <dbe-variable-list name="value" class="col-span-3" variableType="List"></dbe-variable-list>
         </div>
-    `
+        <dbe-list name="elements" title="Elements" modalId="elementsModal" itemTitle="async (item, i) => item.data.get('value') ?? await App.translate('Element', App.selectedLanguage) + ' #' + i"></dbe-list>
+         <template id="elementsModal">
+            <div class="grid grid-cols-4 items-center gap-4">
+                <dbe-label name="Value"></dbe-label>
+                <dbe-variable-list name="value" class="col-span-3" variableType="Any"></dbe-variable-list>
+            </div>
+        </template>    
+`
     static load(context) {
     }
-    static async run({id, data, actionManager, setVariable}) {
-        setVariable(data.get("value"), [])
+    static async run({id, data, actionManager, getVariable, setVariable}) {
+        setVariable(data.get("value"), data.get("elements").map(el => getVariable(el.data.get('value'))))
         actionManager.runNext(id, "action")
     }
 }
