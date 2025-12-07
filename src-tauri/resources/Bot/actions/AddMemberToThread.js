3ac37dde80fabf7a0d5fbb7a22e540371ffe0df5
Caldes Mihai <mihaicaldes91@gmail.com>
2025-12-07 23:09:49 +0200

    bug fix

---
+++ src-tauri/resources/Bot/actions/AddMemberToThread.js
@@ -0,0 +1,28 @@
+export default class AddMemberToThread {
+    static type = "Add Member To Thread";
+
+    static variableTypes = ["Channel", "Member"];
+
+    static html = `
+        <div class="grid grid-cols-4 items-center gap-4">
+            <dbe-label name="Thread"></dbe-label>
+            <dbe-variable-list name="thread" class="col-span-3" variableType="Channel"></dbe-variable-list>
+        </div>
+
+        <div class="grid grid-cols-4 items-center gap-4">
+            <dbe-label name="Member"></dbe-label>
+            <dbe-variable-list name="member" class="col-span-3" variableType="Member"></dbe-variable-list>
+        </div>
+    `;
+
+    static load() {}
+
+    static async run({ id, data, actionManager, getVariable }) {
+        const thread = getVariable(data.get("thread"));
+        const member = getVariable(data.get("member"));
+
+        await thread.members.add(member.id);
+
+        actionManager.runNext(id, "action");
+    }
+}
