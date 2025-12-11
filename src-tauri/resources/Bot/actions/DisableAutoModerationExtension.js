babbf30cd6d8693da3cdc7cbfd39d0c1c3ea6a5a
Caldes Mihai <mihaicaldes91@gmail.com>
2025-12-11 22:58:25 +0200

    bug fix

---
+++ src-tauri/resources/Bot/actions/DisableAutoModerationExtension.js
@@ -0,0 +1,25 @@
+import {Dashboard} from "../classes/Dashboard.js";
+import {Bot} from "../classes/Bot.js";
+
+export default class DisableAutoModerationExtension {
+    static type = "Disable Auto Moderation Extension";
+    static variableTypes = ["Server"]
+    static html = `
+        <div class="grid grid-cols-4 items-center gap-4">
+            <dbe-label name="Server"></dbe-label>
+            <dbe-variable-list name="server" class="col-span-3" variableType="server"></dbe-variable-list>
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+            <dbe-label name="Is disabled?"></dbe-label>
+            <dbe-select name="value" values="True,False" class="col-span-3"></dbe-select>
+        </div>
+    `
+    static load(context) {
+    }
+    static async run({id, data, actionManager, getVariable}) {
+        const server = getVariable(data.get("server"))
+        const disabled = data.get("value") === "True"
+        await Bot.setData(`$AUTOMOD$$$${server.id}`, disabled)
+        actionManager.runNext(id, "action")
+    }
+}
