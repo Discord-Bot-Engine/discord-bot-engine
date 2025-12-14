abde8dbbc19336d88c10b48ce6cc4385f6a03ef1
Caldes Mihai <mihaicaldes91@gmail.com>
2025-12-14 19:56:25 +0200

    dashboard

--- src-tauri/resources/Bot/dashboard/src/lib/components/Navbar.svelte
+++ src-tauri/resources/Bot/dashboard/src/lib/components/Navbar.svelte
@@ -4,13 +4,15 @@
 	const { data } = $props();
 </script>
 
-<div class="flex w-full justify-between gap-1 bg-sidebar p-2 px-3">
-	<div>
+<div class="flex w-full gap-1 bg-sidebar p-2 px-3">
+	<div class="flex w-1/3 gap-1">
 		<img class="h-9 w-9 rounded-xl" src={data.icon} />
 		<a class={buttonVariants({ variant: 'ghost' })} href="/">{data.name}'s dashboard</a>
 	</div>
-	<a class={buttonVariants({ variant: 'ghost' })}>{data.guildCount} Servers</a>
-	{#if data.isAdmin}
-		<a class="{buttonVariants({ variant: 'ghost' })} ml-auto" href="/admin">Edit</a>
-	{/if}
+	<a class="{buttonVariants({ variant: 'ghost' })} mr-auto ml-auto">{data.guildCount} Servers</a>
+	<div class="flex w-1/3 gap-1">
+		{#if data.isAdmin}
+			<a class={buttonVariants({ variant: 'ghost' })} href="/admin">Edit</a>
+		{/if}
+	</div>
 </div>
