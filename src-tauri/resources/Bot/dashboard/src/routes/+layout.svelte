6f39097544a5a88d1fcfde8e970e58521d553e0c
Caldes Mihai <mihaicaldes91@gmail.com>
2025-12-16 11:48:18 +0200

    dashboard

--- src-tauri/resources/Bot/dashboard/src/routes/+layout.svelte
+++ src-tauri/resources/Bot/dashboard/src/routes/+layout.svelte
@@ -1,14 +1,16 @@
 <script>
 	import '../app.css';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import Pages from '$lib/components/Pages.svelte';
 
 	let { children, data } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={data.icon} />
+	<title>{data.name}</title>
 </svelte:head>
-<div class="w-screen h-screen overflow-hidden">
-	<Navbar data={data} />
+<div class="h-screen w-screen overflow-hidden">
+	<Navbar {data} />
 	{@render children?.()}
 </div>
