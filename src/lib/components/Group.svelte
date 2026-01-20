c692d3bd45d4ec8993a00b5d51e416a5763d736c
Caldes Mihai <mihaicaldes91@gmail.com>
2026-01-20 17:35:58 +0200

    bug fix

--- src/lib/components/Group.svelte
+++ src/lib/components/Group.svelte
@@ -26,11 +26,9 @@
 {/if}
     <div class="text-3xl w-full h-full ">
         {#snippet groupSnip(text)}
-<input class="w-full !outline-none text-primary nodrag" placeholder={text} value={data.get("text") ?? ""} noVariables
-       oninput={(evt) => {
+<input class="w-full !outline-none text-primary nodrag" placeholder={text} value={data.get("text") ?? ""} noVariables oninput={(evt) => {
            data.set("text", evt.target.value)
-      }}
-       onfocus={() => {
+      }} onfocus={() => {
           BotManager.selectedBot.markAsModified(App.selectedTrigger.id)
           App.updateUndo()
       }}
