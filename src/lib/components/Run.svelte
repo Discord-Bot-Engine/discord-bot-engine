f829d501f4edd18370cdfaf9e12643c965bca1bc
Caldes Mihai <mihaicaldes91@gmail.com>
2026-02-23 11:27:20 +0200

    bug fix

--- src/lib/components/Run.svelte
+++ src/lib/components/Run.svelte
@@ -16,14 +16,14 @@
         {/if}
     </Button>
     {#if !App.hideConsole}
-<Card.Root class="!rounded-none border-t-0 w-full h-full min-h-50 p-1 relative shadow-none">
-    <Card.Content class="p-1 px-1 overflow-hidden h-full">
+<Card.Root class="!rounded-none border-t-0 w-full h-[30vh] min-h-50 p-1 relative shadow-none">
+    <Card.Content class="p-1 px-1 overflow-hidden h-full flex flex-col">
         <div class="flex justify-end h-fit font-bold">
             <label class="mr-auto text-md"><Translation text="Console"/></label>
             <Button variant={BotManager.selectedBot?.isRunning ? undefined : 'secondary'} size="icon" class="size-8 -mt-1 rounded-lg rounded-r-none" onclick={() => BotManager.runBot()}><PlayIcon size={16} /></Button>
             <Button variant={BotManager.selectedBot?.isRunning ? 'secondary' : undefined} size="icon" class="size-8 -mt-1 rounded-lg rounded-l-none" onclick={() => BotManager.stopBot()}><SquareIcon size={16} /></Button>
         </div>
-        <ScrollArea class="text-sm h-[25vh]">
+        <ScrollArea class="text-sm flex-1 overflow-hidden">
             <pre class="whitespace-pre-wrap">{BotManager.selectedBot?.stdout}</pre>
         </ScrollArea>
     </Card.Content>
